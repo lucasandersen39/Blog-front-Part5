07a0e76adbc93830ece71fe77d575c0bb98aca61
lucasandersen39 <lucas.andersen39@gmail.com>
2026-07-19 00:54:41 -0300

    Blogs fornt-end. Step 1. Added login (5.1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,90 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState('')
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    const credential = {
+      username: username,
+      password: password
+    }
+    try {
+      const resultLogin = await loginService.login(credential)
+      setUser(resultLogin)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+      setErrorMessage(exception?.response?.data?.error)
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => {
+    <form onSubmit={handleLogin}>
+      <div>
+        Username
+        <input type="text" name="username" value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        Password
+        <input type='password' name='password' value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <div>
+        <button type='submit'>Login</button>
+      </div>
+    </form>
+  }
+
+  const blogForm = () => {
+    <form>
+      <div>
+        Title
+        <input type="text" name="title" />
+      </div>
+      <div>
+        Author
+        <input type="text" name="author" />
+      </div>
+      <div>
+        Url
+        <input type="text" name="url" />
+      </div>
+      <div>
+        <button type="submit">Create</button>
+      </div>
+    </form>
+  }
 
   return (
     <div>
+      <div>{errorMessage != '' ? errorMessage : ''}</div>
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged in</p>
+          {blogForm()}
+        </div>
+      }
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
---
+++ src/services/login.js
@@ -0,0 +1,9 @@
+import axios from "axios"
+const baseUrl = 'http://localhost:3001/api'
+
+const login = async ({ username, password }) => {
+    const response = await axios.post(`${baseUrl}/login`, { username, password })
+    return response.data
+}
+
+export default { login }
